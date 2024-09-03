4c4f03e17bccc9782b7a283341b72540764bb659
gabriel <gabrielvalino25@hotmail.com>
2024-09-03 18:17:21 -0300

    chore: add solution and updated_at to our updated ticket

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,7 +1,12 @@
 export function updateStatus({req, res, database}){
   const { id } = req.params
+  const { solution } = req.body
 
-  database.update("tickets", id, {status: "closed"})
+  database.update("tickets", id, { 
+      status: "closed", 
+      solution, 
+      updated_at: new Date() 
+    })
 
   return res.end()
 }
